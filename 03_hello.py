3ce5dd2826684b0d79f10af228c37cbc3d72500e
Likhitha1218 <150022438+Likhitha1218@users.noreply.github.com>
2023-12-03 01:40:13 +0530

    Add files via upload

---
+++ 03_hello.py
@@ -0,0 +1,11 @@
+# print program based question
+'''
+This is solution of CodeWithHarry 
+probem 1 in pratice set1 !
+'''
+#This is also a comment like above line
+
+print('''Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky. ''')
